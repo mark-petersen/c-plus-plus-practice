c3fdfc02505b07c44a8c49c9c2a51b02933caafd
Mark Petersen <mpetersen@lanl.gov>
2022-12-21 10:01:36 -0500

    hello

---
+++ 0_hello_world/hello.cpp
@@ -0,0 +1,21 @@
+// Your First C++ Program
+// compile and run with:
+// g++ hello.cpp
+// ./a.out
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!"<< std::endl;
+    printf("hello again \n");
+    int x;
+    float y, z;
+    int* p;
+    printf("number= %i \n",x);
+    y = 0.0;
+    z = 3/y;
+    printf("z = %f \n",z);
+    printf("p = %i \n",p);
+    return 0;
+}
+
