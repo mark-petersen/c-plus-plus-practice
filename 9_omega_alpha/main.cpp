69b439fd675184b2674f66d5cb0c29e07cd96d29
Mark Petersen <mpetersen@lanl.gov>
2023-08-23 10:37:20 -0400

    add description to main.cpp

--- 9_omega_alpha/main.cpp
+++ 9_omega_alpha/main.cpp
@@ -1,9 +1,20 @@
-/*                                                                                            __            __                 
-    ____    ____ ___    ___    ____ _____ _            ____ _/ /___    / /_    ____ _
+/*                                              __      __         
+  ____  ____ ___  ___  ____ _____ _      ____ _/ /___  / /_  ____ _
  / __ \/ __ `__ \/ _ \/ __ `/ __ `/_____/ __ `/ / __ \/ __ \/ __ `/
-/ /_/ / / / / / /    __/ /_/ / /_/ /_____/ /_/ / / /_/ / / / / /_/ / 
-\____/_/ /_/ /_/\___/\__, /\__,_/            \__,_/_/ .___/_/ /_/\__,_/    
-                                        /____/                                     /_/                                    */
+/ /_/ / / / / / /  __/ /_/ / /_/ /_____/ /_/ / / /_/ / / / / /_/ / 
+\____/_/ /_/ /_/\___/\__, /\__,_/      \__,_/_/ .___/_/ /_/\__,_/  
+                    /____/                   /_/                  */
+
+// Omega-alpha is a preliminary version of the Omega ocean model. The modeling
+// capability is the same as Omega-0: the shallow water equations with
+// identical vertical layers and inactive tracers. Omega-alpha runs on a single
+// cpu with no domain decomposition, no GPU capability, and with native c++
+// vectors rather than YAKL array objects. The purpose is to work out the
+// design of the classes, functions, files, naming conventions, and code style
+// in a very simple setting. The design of all Omega versions and the model
+// equations may be found at:
+// https://github.com/E3SM-Project/Omega/blob/develop/components/omega/doc/design/ShallowWaterOmega0.md
+// Mark Petersen, LANL, August 2023
 
 #include <iostream>
 #include <string>
@@ -33,13 +44,13 @@ int main() {
         for (size_t n=0; n<meta.stateLevelsInMemory; n++) {
             s.push_back(temporaryState);
         }
-    }   // temporaryState is destroyed here.
+    }     // temporaryState is destroyed here.
     {
         Tend temporaryTend(config, m);
         for (size_t n=0; n<meta.tendLevelsInMemory; n++) {
             t.push_back(temporaryTend);
         }
-    }   // temporaryTend is destroyed here.
+    }     // temporaryTend is destroyed here.
     s[0].init(config, m);
 
     //*******************************************************
