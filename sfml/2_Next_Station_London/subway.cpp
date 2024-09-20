b7945c7c617a713d597642ac077738974d2d587d
Mark Petersen <mpetersen@lanl.gov>
2024-09-20 13:52:28 -0600

    tracks viz

--- sfml/2_Next_Station_London/subway.cpp
+++ sfml/2_Next_Station_London/subway.cpp
@@ -7,8 +7,8 @@ int XMax = 1000;
 int iLen = 10;
 int jLen = 10;
 
-int c(float xIn) {
-  int x = (xIn-0.5)*XMax/10.0;
+int c(float iIn) {
+  int x = (iIn+0.5)*XMax/10.0;
   return x;
 }
 
@@ -16,40 +16,44 @@ class Track {
   public:
     //Constructor
     Track() = default;
-    
-    void activate() {
-      // asfdl
+    Track(int i1,int j1,int i2,int j2) :
+      m_i1(i1), m_j1(j1), m_i2(i2), m_j2(j2)
+    {
+      //m_line[0].position = sf::Vector2f(c(i1), c(j1));
+      //m_line[1].position = sf::Vector2f(c(i2), c(j2));
+      //m_line[0].color  = sf::Color::Black;
+      //m_line[1].color  = sf::Color::Black;
     }
-    
-  void viz(sf::RenderWindow &windowIn) {
-    if (!m_exists) return;
-  }
+    void info() {
+      std::cout << "track: ("<<m_i1<<", "<<m_j1<<") to ( "<<m_i2<<", "<<m_j2<<")"<<std::endl;
+    }
+    void viz(sf::RenderWindow &windowIn) {
+      sf::VertexArray m_line(sf::LineStrip, 2);
+      m_line[0].position = sf::Vector2f(c(m_i1), c(m_j1));
+      m_line[1].position = sf::Vector2f(c(m_i2), c(m_j2));
+      m_line[0].color  = sf::Color::Black;
+      m_line[1].color  = sf::Color::Black;
+      windowIn.draw(m_line);
+    }
 
   private: 
-    bool m_exists {false};
     int m_station1 {-1};
     int m_station2 {-1};
     int m_i1 {-1};
     int m_j1 {-1};
     int m_i2 {-1};
     int m_j2 {-1};
-    float m_x1 {-1};
-    float m_x2 {-1};
-    float m_y1 {-1};
-    float m_y2 {-1};
 };
 
 class Station {
   public:
-    int i {-1};
-    int j {-1};
     // Constructor
     Station() = default;
 
-    Station(double x, double y, int shape) 
-      : m_x{x}, m_y{y}, m_shape{shape}
+    Station(int i, int j, int shape) 
+      : m_i{i}, m_j{j}, m_shape{shape}
     {
-      std::cout << "Station constructor called with x="<<x<<" & y="<<y<<std::endl;
+      std::cout << "Station constructor called with i="<<i<<" & j="<<j<<std::endl;
       m_exists = true;
     }
   
@@ -67,7 +71,7 @@ class Station {
       r=18;
       shape.setRadius(r);
       shape.setPointCount(3);
-      shape.setPosition(c(m_x)-r, c(m_y)-r);
+      shape.setPosition(c(m_i)-r, c(m_j)-r);
       shape.setOutlineThickness(4.0);
       shape.setFillColor(sf::Color::White);
       shape.setOutlineColor(sf::Color::Black);
@@ -76,7 +80,7 @@ class Station {
       shape.setRadius(r);
       shape.setPointCount(3);
       shape.setRotation(180);
-      shape.setPosition(c(m_x+2*r), c(m_y));
+      shape.setPosition(c(m_i+2*r), c(m_j));
       shape.setOutlineThickness(4.0);
       shape.setFillColor(sf::Color::White);
       shape.setOutlineColor(sf::Color::Black);
@@ -86,7 +90,7 @@ class Station {
     shape.setRadius(r);
     shape.setPointCount(30);
     shape.setRotation(0);
-    shape.setPosition(c(m_x)-r, c(m_y)-r);
+    shape.setPosition(c(m_i)-r, c(m_j)-r);
     shape.setOutlineThickness(2.0);
     if (!m_home) {
       shape.setFillColor(sf::Color::White);
@@ -112,7 +116,7 @@ class Station {
       shape.setRadius(r);
       shape.setPointCount(30);
       shape.setRotation(45);
-      shape.setPosition(c(m_x), c(m_y)-1.4*r);
+      shape.setPosition(c(m_i), c(m_j)-1.4*r);
     }
 
     // triangle
@@ -121,7 +125,7 @@ class Station {
       shape.setRadius(r);
       shape.setPointCount(3);
       shape.setRotation(0);
-      shape.setPosition(c(m_x)-r, c(m_y)-r);
+      shape.setPosition(c(m_i)-r, c(m_j)-r);
     }
 
     // square
@@ -130,7 +134,7 @@ class Station {
       shape.setRadius(r);
       shape.setPointCount(4);
       shape.setRotation(45);
-      shape.setPosition(c(m_x), c(m_y)-1.4*r);
+      shape.setPosition(c(m_i), c(m_j)-1.4*r);
     }
 
     // pentagon
@@ -139,7 +143,7 @@ class Station {
       shape.setRadius(r);
       shape.setPointCount(5);
       shape.setRotation(0);
-      shape.setPosition(c(m_x)-r, c(m_y)-r);
+      shape.setPosition(c(m_i)-r, c(m_j)-r);
     }
 
     shape.setOutlineThickness(4.0);
@@ -163,15 +167,13 @@ class Station {
 }
 
     void init(int n) {
-      j = n / 10; 
-      i = n - 10*j;
+      m_j = n / 10; 
+      m_i = n - 10*m_j;
       //std::cout << "n = "<<n<<" i = "<<i<<" j = "<<j<<std::endl;
     }
 
     void activate(int shape, int special) {
       m_exists = true;
-      m_x = i + 1;
-      m_y = j + 1;
       m_shape = shape;
       if (special==-1) {
         m_tourist = true;
@@ -183,8 +185,8 @@ class Station {
     void info() {
       if (m_exists) {
         std::cout << "Station exists" << std::endl;
-        std::cout << "x = " << m_x << std::endl;
-        std::cout << "y = " << m_y << std::endl;
+        std::cout << "i = " << m_i << std::endl;
+        std::cout << "j = " << m_j << std::endl;
         std::cout << "shape = " << m_shape << std::endl;
       } else { 
         std::cout << "Station does not exist" << std::endl;
@@ -194,8 +196,8 @@ class Station {
 
   private:
     bool m_exists {false};
-    double m_x {-1.0};
-    double m_y {-1.0};
+    int m_i {-1};
+    int m_j {-1};
     int m_shape {-1};
     bool m_tourist {false};
     int m_home {0};
@@ -218,10 +220,10 @@ void VLine(float xIn, sf::RenderWindow &windowIn, sf::Color colorIn) {
 void Line(float x1, float y1, float x2, float y2, float PW,
   sf::RenderWindow &windowIn, sf::Color colorIn) {
     sf::VertexArray lines(sf::LinesStrip, 4);
-    lines[0].position = sf::Vector2f(c(x1)-PW, c(y1)-PW);
-    lines[1].position = sf::Vector2f(c(x1)-PW, c(y2)+PW);
-    lines[2].position = sf::Vector2f(c(x2)+PW, c(y2)+PW);
-    lines[3].position = sf::Vector2f(c(x2)+PW, c(y1)-PW);
+    lines[0].position = sf::Vector2f(c(x1-1)-PW, c(y1-1)-PW);
+    lines[1].position = sf::Vector2f(c(x1-1)-PW, c(y2-1)+PW);
+    lines[2].position = sf::Vector2f(c(x2-1)+PW, c(y2-1)+PW);
+    lines[3].position = sf::Vector2f(c(x2-1)+PW, c(y1-1)-PW);
     lines[0].color  = colorIn;
     lines[1].color  = colorIn;
     lines[2].color  = colorIn;
@@ -272,8 +274,8 @@ int main()
 
        {  0, 5, 5, 0 }, 
        {  2, 5, 4, 3 }, 
-       {  3, 5, 3, 0 }, 
-       {  5, 5, 0, 0 }, 
+       {  4, 5, 0, 0 }, 
+       {  7, 5, 0, 0 }, 
 
        {  3, 6, 5, 0 }, 
        {  4, 6, 3, 0 }, 
@@ -324,15 +326,16 @@ int main()
     int jInc[8] {0,1,1,1,0,-1,-1,-1};
     std::vector<Track> Tracks;
     //for (int n1=0; n1<100; n1++) {
-    for (int n1=0; n1<10; n1++) {
+    int i; int j; int n;
+    int nTracks = 0;
+    for (int i1=0; i1<iLen; i1++) {
+     for (int j1=0; j1<jLen; j1++) {
+      int n1 = 10*j1+i1;
       if (Stations[n1].exists()) {
-        int i; int j; int n;
-          i = Stations[n1].i;
-          j = Stations[n1].j;
-        std::cout << "* Starting station i,j="<<" ("<<i<<", "<<j<<") "<<std::endl;
+        //std::cout << "* Starting station i,j="<<" ("<<i1<<", "<<j1<<") "<<std::endl;
         for (int rot=0; rot<4; rot++) {
-          i = Stations[n1].i;
-          j = Stations[n1].j;
+          i = i1;
+          j = j1;
           //std::cout << "n1,i,j,rot"<<n1<<" "<<i<<" "<<j<<" "<<rot<<" "<<std::endl;
           for (int iter=0; iter<10; iter++) {
             i += iInc[rot];
@@ -346,15 +349,19 @@ int main()
             n = 10*j + i;
             //std::cout << "iter, n,i,j,rot"<<iter<<" "<<n<<" "<<i<<" "<<j<<" "<<rot<<std::endl;
             if (Stations[n].exists()) {
-              std::cout << "  *    to: i,j="<<" ("<<i<<", "<<j<<") rot = "<<rot<<std::endl;
+              Tracks.push_back(Track(i1,j1,i,j));
+              nTracks +=1;
               break;
             }
           }
         }
+       }
       }
 
     }
-    return 0;
+    for (int k=0; k<nTracks; k++) {
+      Tracks[k].info();
+    }
 
     while (window.isOpen())
     {
@@ -379,6 +386,10 @@ int main()
     Line( 9.5, 9.5,10.5, 9.5, 1, window, sf::Color::Yellow);
     Line( 9.5, 9.5, 9.5,10.5, 1, window, sf::Color::Yellow);
 
+    for (int k=0; k<nTracks; k++) {
+      Tracks[k].viz(window);
+    }
+
     for (int n=0; n<100; n++) {
       Stations[n].viz(window);
     }
@@ -386,4 +397,5 @@ int main()
     window.display();
     }
 
+    return 0;
 }
