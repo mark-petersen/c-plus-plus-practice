13ac5a3fe756881dbd16734f7ba4f891b8cc7500
Mark Petersen <mpetersen@lanl.gov>
2024-10-13 06:45:35 -0600

    creates random numbers

--- 11_shuffle/shuffle.cpp
+++ 11_shuffle/shuffle.cpp
@@ -6,12 +6,12 @@
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 
-int shuffle(int n) {
+auto shuffle(int n) {
   int r[n];// old fashioned vector. Could change to vector container later.
   int a[n];
   for (int j=0; j<n; j++) {
     int rNew = std::rand();
-    std::cout << "rNew " << rNew << std:: endl;
+    //std::cout << "rNew " << rNew << std:: endl;
     r[j] = rNew;
     a[j] = j;
     for (int i=j; i>0; i--) {
@@ -26,14 +26,22 @@ int shuffle(int n) {
     }
 
   }
-  std::cout << "r " << r << std:: endl;
-  std::cout << "a " << a << std:: endl;
-  return n; // need to return the random vector
+  /* std::cout << "r         a " << std:: endl;
+  for (int j=0; j<n; j++) {
+    std::cout << r[j] <<"  " << a[j] << std:: endl;
+  }
+  */
+  return a; // need to return the random vector
 }
 
 int main() {
   /* initialize random seed: */
   srand (time(NULL));
-  int out = shuffle(5);
+  int n=52;
+  auto a = shuffle(n);
+  std::cout << " a " << std:: endl;
+  for (int j=0; j<n; j++) {
+    std::cout << a[j] << std:: endl;
+  }
 }
 
