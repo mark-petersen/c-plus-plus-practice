d8d20e0975801f378740dc271d18f60e49acf3a3
Mark Petersen <mpetersen@lanl.gov>
2024-10-13 06:40:43 -0600

    creates random numbers

--- 11_shuffle/shuffle.cpp
+++ 11_shuffle/shuffle.cpp
@@ -1,12 +1,17 @@
 // shuffle cards
-
 /* Given an integer n, create a vector of n integers in a random order */
+// g++ shuffle.cpp
 // change output:
+#include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
 int shuffle(int n) {
   int r[n];// old fashioned vector. Could change to vector container later.
   int a[n];
   for (int j=0; j<n; j++) {
-    int rNew = j;  // a = rand(); later
+    int rNew = std::rand();
+    std::cout << "rNew " << rNew << std:: endl;
     r[j] = rNew;
     a[j] = j;
     for (int i=j; i>0; i--) {
@@ -25,6 +30,8 @@ int shuffle(int n) {
 }
 
 int main() {
-  int x=2;
+  /* initialize random seed: */
+  srand (time(NULL));
+  int out = shuffle(5);
 }
 
